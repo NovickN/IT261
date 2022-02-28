c31ecfd12bf588cb4c4551e74bf1bdbb29326ece
NovickN <86983647+NovickN@users.noreply.github.com>
2022-02-28 08:18:52 -0800

    Add files via upload

---
+++ IT261/IT261/credentials.php
@@ -0,0 +1,16 @@
+<?php
+//in order to connect to your database, we need the following
+//DB_NAME, DB_PASSWORD, DB_USER, DB_HOST
+// waiving red flag - depending on server company 
+//DB_NAME = u615959180_it261_people
+
+//DB_USER = u615959180_nnovick
+
+//DB_HOST = localhost
+define('DB_NAME', 'u615959180_it261_people');
+define('DB_HOST', 'localhost'); 
+define('DB_USER', 'u615959180_nnovick');
+define('DB_PASSWORD', 'password');
+
+//WHEN UPLOADING TO GITHUB, DO NOT UPLOAD CREDENTIALS
+//USE .gitignore 
